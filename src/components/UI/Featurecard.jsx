c80b4b2d9ba365c347dc17fc5e4564488313bfa1
Saurav <63955601+saurav2701@users.noreply.github.com>
2025-05-05 14:36:53 +0545

    changes in feature card

--- src/components/UI/Featurecard.jsx
+++ src/components/UI/Featurecard.jsx
@@ -3,16 +3,16 @@ import featurecardclasses from './Featurecard.module.css';
 
 const FeatureCard = React.forwardRef((props, ref) => {
 	return (
-		<div
-			ref={ref}
-			style={{
-				backgroundColor: props.bgcolor === 'black' ? '#181818' : null,
-				color: props.bgcolor === 'black' ? 'white' : '#black',
-				zIndex: props.bgcolor === 'black' ? 200 : 100,
-			}}
-			className={featurecardclasses.fcard}>
-			{props.children}
-		</div>
+		// <div
+		// 	ref={ref}
+		// 	style={{
+		// 		backgroundColor: props.bgcolor === 'black' ? '#181818' : null,
+		// 		color: props.bgcolor === 'black' ? 'white' : '#black',
+		// 		zIndex: props.bgcolor === 'black' ? 200 : 100,
+		// 	}}
+		// 	className={featurecardclasses.fcard}>
+		// 	{props.children}
+		// </div>
 	);
 });
 
